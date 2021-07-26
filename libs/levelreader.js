6fa3f255be431166a58f76f257e759ce2820fc2d
konalt <danannlawless@gmail.com>
2021-07-26 01:25:03 +0100

    Fix level finding system

--- libs/levelreader.js
+++ libs/levelreader.js
@@ -5,11 +5,11 @@ function getLevelData(levelname = "unknown", overrideFolder = "levels") {
     if (levelname.split(".")[levelname.split(".").length - 1] !== "json") {
         levelname += ".json"
     }
-    console.log("Searching for level " + path.join("..", overrideFolder, levelname));
+    console.log("Searching for level " + path.join(overrideFolder, levelname));
     try {
-        if (fs.existsSync(path.join("./", "..", overrideFolder, levelname))) {
+        if (fs.existsSync(path.join("./", overrideFolder, levelname))) {
             // level exists, get data and return
-            return JSON.parse(fs.readFileSync(path.join("..", "..", overrideFolder, levelname)).toString());
+            return JSON.parse(fs.readFileSync(path.join("./", overrideFolder, levelname)).toString());
         } else {
             return null;
         }
