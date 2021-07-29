cc713ebec92dbada18d19ca6c91bbc9c4672857a
konalt <79701511+konalt@users.noreply.github.com>
2021-07-29 10:37:29 +0100

    idfk more collision i guess

--- libs/entity.js
+++ libs/entity.js
@@ -3,23 +3,40 @@ const { Direction } = require("./direction");
 
 class Entity {
     constructor(name = "Unknown Entity " + Math.ceil(Math.random() * 100)) {
+        // Gotta be lowercase for console compatibility.
         this.name = name.toLowerCase();
+        // Default position. I should make this customizable at some point
         this.x = 0;
         this.y = 0;
+        // Default sprite so it actually renders something
         this.sprite = "?";
+        // To hide it? Idk why I put this in, it doesn't even do anything yet. (as of 29/07/2021 10:30am)
         this.visible = true;
+        // Bool to make sure not to render if out of bounds.
         this.isOutOfBounds = false;
+        // Basic collision bool. Mainly for the level editor, but this can be used for other things too
         this.enableCollision = true;
+        // This creates a circular. Too bad.
         this.currentRenderer;
+        // Make this an object so it's easier to refer to it with just this.collideFunctions[ent.name]
+        this.collideFunctions = {};
     }
     setPosition(x, y) {
+        // what
         this.x = x;
         this.y = y;
     }
     move(moveDirection) {
         // Clone x and y for collision purposes
+        // Return of the collision system in x-maze 2 i guess
         var xb = JSON.parse(JSON.stringify({ x: this.x })).x;
         var yb = JSON.parse(JSON.stringify({ y: this.y })).y;
+        // Grid to help with remembering what direction these actually go
+        //   X-      X+
+        // Y-
+        //     swag
+        //
+        // Y+
         switch (moveDirection) {
             case Direction.UP:
                 yb--;
@@ -56,6 +73,16 @@ class Entity {
     }
     setVisible(visible) {
         this.visible = visible;
+    }
+    setOnCollideWith(ent, callback) {
+        // On entity collisions. Currently have no clue how to handle this, maybe with the renderer?
+        // Maybe it checks every frame? Who knows. Not me, anyway.
+        if (this.currentRenderer.entities[ent.name]) {
+            this.collideFunctions[ent.name] = callback;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
